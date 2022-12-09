eceb9d2deed2303701c18794cde7cdf265a465b1
GIBeom-Cheon <110073263+GIBeom-Cheon@users.noreply.github.com>
2022-12-09 12:23:22 +0900

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -34,7 +34,7 @@ const ContactForm = () => {
         <Form.Group  xs className="mb-3" controlId="formContact">
           <Form.Label className={style.white}>Phone Number</Form.Label>
           <Form.Control
-            type="password"
+            type="number"
             placeholder="Enter your phone number"
             onChange={(e) => setPhoneNumber(e.target.value)}
             value={phoneNumber}
